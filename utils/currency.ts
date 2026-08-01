74efa49319722fdb24819c77e9621ea27711993f
saikiran2048 <saikirandasari314@gmail.com>
2026-08-01 11:02:50 +0530

    typo

--- utils/currency.ts
+++ utils/currency.ts
@@ -1,3 +1,4 @@
+/** Parses a display string like "$1,500" into a number. */
 export function parsePrice(text: string | null): number {
   if (!text) return 0;
   return parseFloat(text.replace(/[$,]/g, ''));
